32b459ee80a501dceaf4e17c815a3134961cca83
Mauricio Paiva <mpaiva1@its.jnj.com>
2017-04-26 21:07:50 -0300

    Changing PHP version

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+
+if [ -z "$1" ]
+then
+      TAG=''
+else
+      TAG=":$1"
+fi
+
 if [ `docker images -q gragonmau/armstrong-base` ]; then
   echo 'There is already image assigned to this tag. Rebuilding...'
   docker rmi -f gragonmau/armstrong-base
 fi
 
-echo 'Building image...'  
-docker build -t gragonmau/armstrong-base .
+echo 'Building image...' 
+docker build -t gragonmau/armstrong-base"$TAG" .
 echo 'Pushing image to Docker Hub'
 docker push gragonmau/armstrong-base
